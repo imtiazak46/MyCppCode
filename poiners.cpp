8383357fe780d8d30e5c2b62c25ff93bb3645c50
imtiazak46 <158585204+imtiazak46@users.noreply.github.com>
2024-04-30 13:54:30 +0500

    incremented address.cpp

--- poiners.cpp
+++ poiners.cpp
@@ -8,9 +8,19 @@ int main(){
 	
 	cout << "this address of a  " << &a << endl << " this pointer value  " << *ptr;
  	// this address of a  0x6ffe04
- 	// this pointer value  10
+ 	// this pointer value  10 
+	
 	// here we used direct pointer stored the value 
 	*ptr = 20 ;
 	cout << *ptr << endl ;
 	// 20
+	
+	cout <<  ptr << endl ; 
+	// 0x6ffe14
+	
+	// here we increment the addres 
+	ptr++ ;
+	cout << " incremented now address is  " << ptr << endl ;
+	//  incremented now address is  0x6ffe18
+	// here is four bytes differe because Int take four bytes 
 }
