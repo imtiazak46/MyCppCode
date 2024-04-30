87525f12af125ad6f33dcb38127e6f936acd5aa9
imtiazak46 <158585204+imtiazak46@users.noreply.github.com>
2024-04-30 13:39:09 +0500

    here we updated the pointer value

--- poiners.cpp
+++ poiners.cpp
@@ -7,6 +7,10 @@ int main(){
 	int* ptr = &a;
 	
 	cout << "this address of a  " << &a << endl << " this pointer value  " << *ptr;
-  // this address of a  0x6ffe04
- // this pointer value  10
+ 	// this address of a  0x6ffe04
+ 	// this pointer value  10
+	// here we used direct pointer stored the value 
+	*ptr = 20 ;
+	cout << *ptr << endl ;
+	// 20
 }
